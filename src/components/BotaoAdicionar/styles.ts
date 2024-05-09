ecd7a6aaac1dca05ea7ade6c3e7d8c1d5c7fe4d3
Amanda Moura <mouraamanda168@gmail.com>
2024-05-09 16:58:13 -0300

    finalizando projeto

---
+++ src/components/BotaoAdicionar/styles.ts
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import variaveis from '../../styles/variaveis'
+
+export const Circulo = styled(Link)`
+  height: 55px;
+  width: 55px;
+  background-color: ${variaveis.verde};
+  color: #fff;
+  position: fixed;
+  bottom: 20px;
+  right: 40px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 40px;
+  text-decoration: none;
+`
